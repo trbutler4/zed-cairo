e04f80ac824ccf4eac74a7a0917f2425ea3a5879
Thomas Butler <trbiv@proton.me>
2024-06-03 20:17:56 -0500

    initial attempt at scarb integration

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use zed_extension_api as zed;
+
+struct CairoExtension {}
+
+impl zed::Extension for CairoExtension {
+    // ...
+    fn new() -> Self {
+        CairoExtension {}
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> zed::Result<zed::Command> {
+        let lsp_args = vec!["--stdio".into()];
+        let (command, args) = match worktree.which("scarb") {
+            Some(command) => (command, lsp_args),
+            None => ("scarb".into(), lsp_args), // TODO: handle this case
+        };
+        Ok(zed::Command {
+            command,
+            args,
+            env: Default::default(),
+        })
+    }
+}
+
+zed::register_extension!(CairoExtension);
