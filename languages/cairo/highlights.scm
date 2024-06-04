19ff4cce2ca2011899d3e1c6b73fde9058816bb2
Thomas Butler <trbiv@proton.me>
2024-06-04 02:10:47 -0500

    random ass scheme files, adding example repo

--- languages/cairo/highlights.scm
+++ languages/cairo/highlights.scm
@@ -1,78 +1,98 @@
 
+; --------------- Identifiers ------------------
+(identifier) @variable
+
+; --------------- Literals ---------------------
+[
+    (string)
+] @string
+
+[
+    (number_literal)
+] @number
+
+
+; --------------- Definitions ------------------
+(fn_definition
+    name: (identifier) @function)
+
+;[
+;    (keyword_true)
+;    (keyword_false)
+;] @boolean
 
 ; keywords
-(meta_type_expression "type" @keyword)
 
-[
-  "as",
-  "break",
-  "const",
-  "continue",
-  "else",
-  "enum",
-  "extern",
-  "false",
-  "fn",
-  "if",
-  "impl",
-  "implicits",
-  "let",
-  "loop",
-  "match",
-  "mod",
-  "mut",
-  "nopanic",
-  "of",
-  "pub",
-  "ref",
-  "return",
-  "struct",
-  "trait",
-  "true",
-  "type",
-  "use",
-  "while",
-] @keyword
+; [
+;   "as",
+;   "break",
+;   "const",
+;   "continue",
+;   "else",
+;   "enum",
+;   "extern",
+;   "false",
+;   "fn",
+;   "if",
+;   "impl",
+;   "implicits",
+;   "let",
+;   "loop",
+;   "match",
+;   "mod",
+;   "mut",
+;   "nopanic",
+;   "of",
+;   "pub",
+;   "ref",
+;   "return",
+;   "struct",
+;   "trait",
+;   "true",
+;   "type",
+;   "use",
+;   "while",
+; ] @keyword
 
 ; 'loose' keywords
-["self", "super"] @keyword.loose
+; ["self", "super"] @keyword.loose
 
 ; 'reservered' keywords
-[
-  "Self",
-  "do",
-  "dyn",
-  "for",
-  "hint",
-  "in",
-  "macro",
-  "move",
-  "static_assert",
-  "static",
-  "try",
-  "typeof",
-  "unsafe",
-  "where",
-  "with",
-  "yield"
-] @keyword.reserved
+; [
+;   "Self",
+;   "do",
+;   "dyn",
+;   "for",
+;   "hint",
+;   "in",
+;   "macro",
+;   "move",
+;   "static_assert",
+;   "static",
+;   "try",
+;   "typeof",
+;   "unsafe",
+;   "where",
+;   "with",
+;   "yield"
+; ] @keyword.reserved
 
-; Built-in functions
-[
-  "assert",
-  "panic"
-] @function.builtin
+; ; Built-in functions
+; [
+;   "assert",
+;   "panic"
+; ] @function.builtin
 
-; Operators
-[
-  "!",
-  "~",
-  "!=",
-  "%",
-  "%=",
-  "&",
-  "&&",
-  "*",
-  "*=",
-  "@",
-] @operator
+; ; Operators
+; [
+;   "!",
+;   "~",
+;   "!=",
+;   "%",
+;   "%=",
+;   "&",
+;   "&&",
+;   "*",
+;   "*=",
+;   "@",
+; ] @operator
---
+++ languages/cairo/outline.scm
@@ -0,0 +1,1 @@
+(fn_definition (identifier) @name) @item
